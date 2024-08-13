319b057f56d9fe9144e57ec91542523b1ef32453
KimYonai <msb0714@gmail.com>
2024-08-13 13:08:01 +0900

    Feat/NewClass
    다음의 항목이 추가되었습니다.
    -ForestScene 클래스 추가
    -CanyonScene 클래스 추가

---
+++ OOPConsoleProject/Scenes/CanyonScene.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject.Scenes
+{
+    public class CanyonScene : Scene
+    {
+        public CanyonScene(Game game) : base(game) { }
+
+        public override void Enter()
+        {
+
+        }
+        public override void Render()
+        {
+
+        }
+        public override void Input()
+        {
+
+        }
+        public override void Update()
+        {
+
+        }
+        public override void Exit()
+        {
+
+        }
+    }
+}
